f3aba642b1fc14bf3f30c2a7404a4b07828dd85d
Luke Ponga <developmentdesignsltd@gmail.com>
2025-11-20 13:57:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3331756920-dfce2",
+  "appId": "1:602154147388:web:60284e3d1dac64d0e4ad39",
+  "apiKey": "AIzaSyCNPhJPSMZgVlQN-0pyuTrQrHD2EMIbQdQ",
+  "authDomain": "studio-3331756920-dfce2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "602154147388"
+};
